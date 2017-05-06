bc7e6381927b6c1c14a99c01267957f62eca2618
dhruszecki <dariohruszecki@gmail.com>
2017-05-06 08:56:47 -0300

    Create LeerDeTeclado.java
    initial

---
+++ src/main/java/lenguaje/LeerDeTeclado.java
@@ -0,0 +1,20 @@
+package cajero;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daro
+ */
+public class Cajero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena = sc.nextLine();
+        System.out.println("ingreso: "+cadena);
+    }
+    
+}
